ce2ead171140dc347a10041456c00abe596ed1b1
namjberta <matheus.j.bertazzo@exxonmobil.com>
2019-11-01 19:00:45 -0300

    initial commit

---
+++ orderprovider/src/main/java/com/xom/orderprovider/OrderproviderApplication.java
@@ -0,0 +1,13 @@
+package com.xom.orderprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderproviderApplication.class, args);
+    }
+
+}
---
+++ orderprovider/src/test/java/com/xom/orderprovider/OrderproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xom.orderprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
