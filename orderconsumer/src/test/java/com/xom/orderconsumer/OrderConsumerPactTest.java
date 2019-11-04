8cff27869899c41b3734c96b85d06d040c8687ca
namjberta <matheus.j.bertazzo@exxonmobil.com>
2019-11-04 14:11:48 -0300

    Change filter format on orders by parameters test

--- orderconsumer/src/test/java/com/xom/orderconsumer/OrderConsumerPactTest.java
+++ orderconsumer/src/test/java/com/xom/orderconsumer/OrderConsumerPactTest.java
@@ -18,6 +18,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(PactConsumerTestExt.class)
 public class OrderConsumerPactTest {
+    @Pact(provider = "OrderAPI", consumer="orderConsumer")
+    public RequestResponsePact createPactForOrdersByParametersEndPoint(PactDslWithProvider builder) {
+        return builder
+                .given("It has orders in AP stripes instance")
+                .uponReceiving("a request filtering orders by AP stripes instace")
+                .path("/orders")
+                .query("[filters][stripesInstance]=ap")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body("{}")
+                .toPact();
+    }
 
     @Pact(provider = "OrderAPI", consumer="orderConsumer")
     public RequestResponsePact createPactForOrdersByIdEndPoint(PactDslWithProvider builder) {
@@ -28,39 +41,25 @@ public class OrderConsumerPactTest {
         return builder
                 .given("It has orders")
                 .uponReceiving("a request for the order ap-1234567890")
-                    .path("/orders/ap-1234567890")
-                    .method("GET")
-                .willRespondWith()
-                    .status(200)
-                    .body(body)
-                .toPact();
-    }
-
-    @Pact(provider = "OrderAPI", consumer="orderConsumer")
-    public RequestResponsePact createPactForOrdersByParametersEndPoint(PactDslWithProvider builder) {
-        return builder
-                .given("It has orders in AP stripes instance")
-                .uponReceiving("a request filtering orders by AP stripes instace")
-                .path("/orders")
-                .query("filters[stripesInstance]=ap")
+                .path("/orders/ap-1234567890")
                 .method("GET")
                 .willRespondWith()
                 .status(200)
-                .body("{}")
+                .body(body)
                 .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "createPactForOrdersByParametersEndPoint")
+    void testForOrdersByParameters(MockServer mockServer) throws IOException {
+        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/orders?[filters][stripesInstance]=ap").execute().returnResponse();
+        assertEquals(200, httpResponse.getStatusLine().getStatusCode());
     }
 
     @Test
     @PactTestFor(pactMethod = "createPactForOrdersByIdEndPoint")
     void testForOrdersById(MockServer mockServer) throws IOException {
         HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/orders/ap-1234567890").execute().returnResponse();
-        assertEquals(200, httpResponse.getStatusLine().getStatusCode());
-    }
-
-    @Test
-    @PactTestFor(pactMethod = "createPactForOrdersByParametersEndPoint")
-    void testForOrdersByParameters(MockServer mockServer) throws IOException {
-        HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/orders?filters[stripesInstance]=ap").execute().returnResponse();
         assertEquals(200, httpResponse.getStatusLine().getStatusCode());
     }
 }
